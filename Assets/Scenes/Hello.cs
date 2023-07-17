8e1b383e25b02d6cb53d4123a13745872609ff38
CatTortoise <eiyi6688@gmail.com>
2023-07-17 19:56:13 +0300

    lockt

--- Assets/Scenes/Hello.cs
+++ Assets/Scenes/Hello.cs
@@ -10,7 +10,7 @@ public class Hello : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        _text.text = "Hello";
+        _text.text = "Hello me";
     }
 
 }
