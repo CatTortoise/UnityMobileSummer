d4d298b804b97d65331800dc9aa4545516267df6
NeoThePotato <neorayb@gmail.com>
2023-08-23 00:43:32 +0300

    Major overhaul to the "Ducky.unity" scene.

---
+++ Assets/Script/InputManager.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    [SerializeField] private Camera _mainCamera;
+    [SerializeField] private LocationMarker _marker;
+
+    void Update()
+    {
+        Touch[] touches = Input.touches;
+        foreach (Touch touch in touches)
+        {
+            if (touch.phase == TouchPhase.Began)
+            {
+                Vector3 vec = _mainCamera.ScreenToWorldPoint(touch.position);
+				_marker.transform.position = new Vector3(vec.x, vec.y, 0);
+                _marker.ChangeLocation(_marker.transform.position);
+            }
+        }
+    }
+}
--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
    //[SerializeField] private PlayerNumber playerNumber;
-    [SerializeField] private Rigidbody2D playerRigidbody;
-    [SerializeField] private LocationMarkerScript locationMarker;
-    [SerializeField] private AnimationController﻿ animationController;
-    [SerializeField] private float moveSpeed;
-    [SerializeField] private float jumpSpeed;
+    [SerializeField] private Rigidbody2D _playerRigidbody;
+    [SerializeField] private LocationMarker _locationMarker;
+    [SerializeField] private AnimationController﻿ _animationController;
+    [SerializeField] private float _moveSpeed;
+    [SerializeField] private float _jumpSpeed;
     private bool _movingFlag = false;
     private bool _isMovingRight = true;
 
@@ -17,35 +17,35 @@ public class PlayerController : MonoBehaviour
     {
         Vector2 moveTo;// = Vector2.Lerp(transform.position,new(locationMarker.transform.position.x,transform.position.y), moveSpeed * Time.deltaTime);
                        //  playerRigidbody.AddForce((moveTo - new Vector2(transform.position.x, transform.position.y)) * moveSpeed * Time.deltaTime,ForceMode2D.Force);
-        if (locationMarker.isActiveAndEnabled)
+        if (_locationMarker.isActiveAndEnabled)
         {
-            Vector3 locationDelta = locationMarker.transform.position - transform.position;
+            Vector3 locationDelta = _locationMarker.transform.position - transform.position;
             locationDelta = locationDelta.normalized;
 
             if (!_movingFlag)
             {
-                animationController.OnIdle();
+                _animationController.OnIdle();
                 _movingFlag = true;
             }
 
             if (_isMovingRight != (0 > locationDelta.normalized.x))
             {
                 _isMovingRight = (0 > locationDelta.normalized.x);
-                animationController.OnChangeDirection(_isMovingRight);
+                _animationController.OnChangeDirection(_isMovingRight);
             }
             moveTo = new(locationDelta.x, 0);
-            playerRigidbody.AddForce(moveTo * moveSpeed * Time.fixedDeltaTime, ForceMode2D.Force);
+            _playerRigidbody.AddForce(moveTo * _moveSpeed * Time.fixedDeltaTime, ForceMode2D.Force);
         }
         else if (_movingFlag)
         {
-            animationController.OnIdle();
+            _animationController.OnIdle();
             _movingFlag = false;
         }
     }
 
     public void Jump()
     {
-        playerRigidbody.AddForce(Vector2.up * jumpSpeed, ForceMode2D.Impulse);
+        _playerRigidbody.AddForce(Vector2.up * _jumpSpeed, ForceMode2D.Impulse);
     }
 
     public void Throw()
@@ -55,7 +55,7 @@ public class PlayerController : MonoBehaviour
 
     private void OnValidate()
     {
-        playerRigidbody = GetComponent<Rigidbody2D>();
-        animationController = GetComponentInChildren<AnimationController>();
+        _playerRigidbody = GetComponentInChildren<Rigidbody2D>();
+        _animationController = GetComponentInChildren<AnimationController>();
     }
 }
